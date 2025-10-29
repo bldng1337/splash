cf8332b75941775dc391a53ecfee53ad62c0ee6b
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2025-10-29 11:53:14 +0100

    Update W2 river

--- content/W2/river/spawner.gd
+++ content/W2/river/spawner.gd
@@ -2,13 +2,24 @@ extends Node2D
 
 @export var entity: PackedScene
 @export var spawn_range=100.0
-@export var amount=10
+@export var amount=3.5
 @export var path: Path2D
+@export var delay=0.8
+
+var _timer: float = 0.0
+var _spawned: int = 0
 
 func _ready():
-	for i in range(amount):
-		var e: Node2D = entity.instantiate()
-		e.position = Vector2(randf_range(-spawn_range, spawn_range), randf_range(-spawn_range, spawn_range))
-		e.path=path
-		e.z_index=1
-		add_child(e)
+	amount=floor(amount*randf_range(0,1))
+
+func _process(delta):
+	_timer += delta
+	if _timer > delay:
+		_timer = 0.0
+		_spawned += 1
+		if _spawned < amount:
+			var e: Node2D = entity.instantiate()
+			e.position = Vector2(randf_range(-spawn_range, spawn_range), randf_range(-spawn_range, spawn_range))
+			e.path=path
+			e.z_index=1
+			add_child(e)
