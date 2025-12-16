1318d16f365ea3c57505cf962c99f9f6c75b182e
cutebonsai <72458411+cutebonsai@users.noreply.github.com>
2025-12-16 10:46:25 +0000

    Deploying to gh-pages from @ bldng1337/splash@e6ec983173df724f00704ba11d0413d0bb8e70e9 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1765873957|5791005';
+const CACHE_VERSION = '1765881975|5894496';
 /** @type {string} */
 const CACHE_PREFIX = 'Platschparty-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
