f7a0f46b798a3426da7e33bdab7adaec02658908
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2025-12-17 14:04:27 +0100

    fix: enable all games

--- general/GameManager.gd
+++ general/GameManager.gd
@@ -2,10 +2,10 @@ class_name GameManager
 extends Node
 
 @export var games: Array[PackedScene]=[
-	# preload("res://content/W2/L2/main.tscn"),
+	preload("res://content/W2/L2/main.tscn"),
 	preload("res://content/W2/river/main.tscn"),
 	preload("res://content/W1/L1/main.tscn"),
-	# preload("res://content/W3/L1/main.tscn"),
+	preload("res://content/W3/L1/main.tscn"),
 ]
 
 var current_game: Node
