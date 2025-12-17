e3821eb3bfa54b7490dfc7c43ea7e5de6dac3372
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2025-12-17 13:27:08 +0000

    Deploying to gh-pages from @ bldng1337/splash@6cc18c8887d673e049bc2dd69fca3a45a134a4a4 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1765976743|7606276';
+const CACHE_VERSION = '1765978017|7365134';
 /** @type {string} */
 const CACHE_PREFIX = 'Platschparty-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
