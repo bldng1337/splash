ed0da60cc417672c28b1d3a525c006a184d21c1d
lisamsklv <147833286+lisamsklv@users.noreply.github.com>
2026-01-01 16:37:26 +0000

    Deploying to gh-pages from @ bldng1337/splash@76f9dcb914699b3d71bb9f1cd6ec6b50f54f0aa1 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1766145469|9138232';
+const CACHE_VERSION = '1767285435|9808857';
 /** @type {string} */
 const CACHE_PREFIX = 'Platschparty-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
